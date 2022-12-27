630604276b63f8ea8da345c395c7b4481db508a6
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-12-27 12:23:03 +0900

    update

--- Project_advise_clothes_front/src/Login/isLogin.js
+++ Project_advise_clothes_front/src/Login/isLogin.js
@@ -1,10 +1,20 @@
 import React from "react";
+<<<<<<< Updated upstream
 import { Link, Route, Switch } from 'react-router-dom';
 import { useCookies } from "react-cookie";
 
 function isLogin() {
     // return !!localStorage.getItem("account");    cookie 활용을 위해 제거
     return document.cookie.split(';').filter(x => x.split("=")[0].trim() === 'auth').length != 0;
+=======
+import { Cookies, useCookies } from "react-cookie";
+
+function isLogin() {
+    // const [cookies, setCookies, removeCookie] = (['info', 'auth']);
+
+    return !!localStorage.getItem('account');
+    // return !!cookies.auth;
+>>>>>>> Stashed changes
 };
 
 export default isLogin;
