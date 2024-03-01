de1869c97001bb6f08fdc92d16b00283f07b1111
ImRieul <rieul.im@gmail.com>
2024-03-01 14:32:09 +0900

    origin uri 수정

--- Server_backend/src/main/java/com/advise_clothes/backend/ServerBackendApplication.java
+++ Server_backend/src/main/java/com/advise_clothes/backend/ServerBackendApplication.java
@@ -18,9 +18,13 @@ public class ServerBackendApplication implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://52.79.195.60",
-                        "http://ec2-52-79-195-60.ap-northeast-2.compute.amazonaws.com",
-                        "http://localhost:3000")
+                // 2023년
+//                .allowedOrigins("http://52.79.195.60",
+//                        "http://ec2-52-79-195-60.ap-northeast-2.compute.amazonaws.com",
+//                        "http://localhost:3000")
+                // 2024년
+                // TODO: 개발용, 서버용 따로 나누기
+                .allowedOrigins("https://web-advise-clothes-front-am952nlt1gbj1t.sel5.cloudtype.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
 
         // .allowedOrigins만 막 추가하다가 에러가 났다. 마지막 설정 값(localhost:3000였음)을 가져오나보다
