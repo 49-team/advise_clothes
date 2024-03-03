90be2428a1b8985248f028975262bf2b36148ac1
ImRieul <rieul.im@gmail.com>
2024-03-04 00:49:48 +0900

    cors().disable() 설정 테스트

--- Server_backend/src/main/java/com/advise_clothes/backend/config/SecurityConfig.java
+++ Server_backend/src/main/java/com/advise_clothes/backend/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors().disable()
                 .httpBasic().disable()
                 .csrf().disable();
     }
